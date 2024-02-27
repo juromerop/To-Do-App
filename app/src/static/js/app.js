0d70576b7275c8372e7ca22231d3cdb7161053c1
juromerop <juromerop@unal.edu.co>
2024-02-27 10:32:29 -0500

    changing color

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -102,7 +102,7 @@ function AddItemForm({ onNewItem }) {
                 <InputGroup.Append>
                     <Button
                         type="submit"
-                        variant="success"
+                        variant="primary"
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
